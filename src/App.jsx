51a0c525443b07de5fed5820b3a0620cf3692ba4
DimitryGrig <dimasgrig1575@gmail.com>
2025-09-15 14:32:53 +0300

    fx

--- src/App.jsx
+++ src/App.jsx
@@ -2,99 +2,99 @@ import { useEffect, useState } from "react";
 import { ControlPanel, Todo } from "./components";
 import { createTodo, readTodos, updateTodo, deleteTodo } from "./api";
 import {
-	addTodoInTodos,
-	findTodo,
-	removeTodoInTodos,
-	setTodoInTodos,
+    addTodoInTodos,
+    findTodo,
+    removeTodoInTodos,
+    setTodoInTodos,
 } from "./utils";
 import { NEW_TODO_ID } from "./constants";
 import styles from "./app.module.css";
 
-function App()  {
-	const [todos, setTodos] = useState([]);
-	const [searchPhrase, setSearchPhrase] = useState("");
-	const [isAlphabetSorting, setIsAlphabetSorting] = useState(false);
+function App() {
+    const [todos, setTodos] = useState([]);
+    const [searchPhrase, setSearchPhrase] = useState("");
+    const [isAlphabetSorting, setIsAlphabetSorting] = useState(false);
 
-	const onTodoAdd = () => {
-		setTodos(addTodoInTodos(todos));
-	};
+    const updateTodos = (updater) => {
+        setTodos((prevTodos) => updater(prevTodos));
+    };
 
-	const onTodoSave = (todoId) => {
-		const { title, completed } = findTodo(todos, todoId) || {};
+    const onTodoAdd = () => {
+        updateTodos((todos) => addTodoInTodos(todos));
+    };
 
-		if (todoId === NEW_TODO_ID) {
-			createTodo({ title, completed }).then((id) => {
-				let updatedTodos = setTodoInTodos(todos, {
-					id: NEW_TODO_ID,
-					isEditing: false,
-				});
-				updatedTodos = removeTodoInTodos(updatedTodos, NEW_TODO_ID);
-				updatedTodos = addTodoInTodos(updatedTodos, {id, title, completed});
-				setTodos(updatedTodos);
-			});
-		} else {
-			updateTodo({ id: todoId, title, completed }).then(() => {
-				setTodos(
-					setTodoInTodos(todos, { id: todoId, isEditing: false }),
-				);
-			});
-		}
-	};
+    const onTodoSave = (todoId) => {
+        const { title, completed } = findTodo(todos, todoId) || {};
 
-	const onTodoEdit = (id) => {
-		setTodos(setTodoInTodos(todos, { id, isEditing: true }));
-	};
+        if (todoId === NEW_TODO_ID) {
+            createTodo({ title, completed }).then((id) => {
+                updateTodos((prevTodos) => {
+                    const updatedTodos = setTodoInTodos(prevTodos, {
+                        id: NEW_TODO_ID,
+                        isEditing: false,
+                    });
+                    return addTodoInTodos(removeTodoInTodos(updatedTodos, NEW_TODO_ID), { id, title, completed });
+                });
+            });
+        } else {
+            updateTodo({ id: todoId, title, completed }).then(() => {
+                updateTodos((prevTodos) => setTodoInTodos(prevTodos, { id: todoId, isEditing: false }));
+            });
+        }
+    };
 
-	const onTodoTitleChange = (id, newTitle) => {
-		setTodos(setTodoInTodos(todos, { id, title: newTitle }));
-	};
+    const onTodoEdit = (id) => {
+        updateTodos((todos) => setTodoInTodos(todos, { id, isEditing: true }));
+    };
 
-	const onTodoCompletedChange = (id, newCompleted) => {
-		const { title } = findTodo(todos, id) || {};
-		updateTodo({ id, title, completed: newCompleted }).then(() => {
-			setTodos(setTodoInTodos(todos, { id, completed: newCompleted }));
-		});
-	};
+    const onTodoTitleChange = (id, newTitle) => {
+        updateTodos((todos) => setTodoInTodos(todos, { id, title: newTitle }));
+    };
 
-	const onTodoRemove = (id) => {
-		deleteTodo(id).then(() => setTodos(removeTodoInTodos(todos, id)));
-	};
+    const onTodoCompletedChange = (id, newCompleted) => {
+        const { title } = findTodo(todos, id) || {};
+        updateTodo({ id, title, completed: newCompleted }).then(() => {
+            updateTodos((todos) => setTodoInTodos(todos, { id, completed: newCompleted }));
+        });
+    };
 
-	useEffect(() => {
-		readTodos(searchPhrase, isAlphabetSorting).then((loadedTodos) =>
-			setTodos(loadedTodos),
-		);
-	}, [searchPhrase, isAlphabetSorting]);
+    const onTodoRemove = (id) => {
+        deleteTodo(id).then(() => updateTodos((todos) => removeTodoInTodos(todos, id)));
+    };
 
-	return (
-		<div className={styles.app}>
-			<ControlPanel
-				onTodoAdd={onTodoAdd}
-				onSearch={setSearchPhrase}
-				onSorting={setIsAlphabetSorting}
-			/>
-			<div>
-				{todos.map(({ id, title, completed, isEditing = false }) => (
-					<Todo
-						key={id}
-						id={id}
-						title={title}
-						completed={completed}
-						isEditing={isEditing}
-						onEdit={() => onTodoEdit(id)}
-						onTitleChange={(newTitle) =>
-							onTodoTitleChange(id, newTitle)
-						}
-						onCompletedChange={(newCompleted) =>
-							onTodoCompletedChange(id, newCompleted)
-						}
-						onSave={() => onTodoSave(id)}
-						onRemove={() => onTodoRemove(id)}
-					/>
-				))}
-			</div>
-		</div>
-	);
+    useEffect(() => {
+        let isMounted = true;
+        readTodos(searchPhrase, isAlphabetSorting).then((loadedTodos) => {
+            if (isMounted) setTodos(loadedTodos);
+        });
+        return () => { isMounted = false; };
+    }, [searchPhrase, isAlphabetSorting]);
+
+    return (
+        <div className={styles.app}>
+            <ControlPanel
+                onTodoAdd={onTodoAdd}
+                onSearch={setSearchPhrase}
+                onSorting={setIsAlphabetSorting}
+            />
+            <div>
+                {todos.map(({ id, title, completed, isEditing = false }) => (
+                    <Todo
+                        key={id}
+                        id={id}
+                        title={title}
+                        completed={completed}
+                        isEditing={isEditing}
+                        onEdit={() => onTodoEdit(id)}
+                        onTitleChange={(newTitle) => onTodoTitleChange(id, newTitle)}
+                        onCompletedChange={(newCompleted) => onTodoCompletedChange(id, newCompleted)}
+                        onSave={() => onTodoSave(id)}
+                        onRemove={() => onTodoRemove(id)}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
